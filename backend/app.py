bc02fa129d2abf730681dd21cc583e513e3b8488
Robert Gleim <rgleim@outlook.com>
2026-03-27 12:58:34 -0500

    saving

--- backend/app.py
+++ backend/app.py
@@ -122,3 +122,4 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     debug = os.getenv('FLASK_ENV') != 'production'
     app.run(debug=debug, host='0.0.0.0', port=port)
+# ....
